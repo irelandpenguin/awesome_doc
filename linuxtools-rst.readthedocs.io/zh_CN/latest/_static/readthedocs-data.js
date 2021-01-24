a4ad97c8a741158ae74693eeec0a3958d92e2dfa
irelandpenguin <irelandspider@gmail.com>
2021-01-24 13:21:39 +0800

    commit linuxtools doc

---
+++ linuxtools-rst.readthedocs.io/zh_CN/latest/_static/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "linuxtools-rst",
+    version: "latest",
+    language: "zh_CN",
+    subprojects: {},
+    canonical_url: "http://linuxtools-rst.readthedocs.io/zh_CN/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/./",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "934c7b3a"
+  };
+  
